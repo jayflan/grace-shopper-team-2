11348cf6c39cc70b2a1c7998f70531df1e644065
jayflan <j.c.flanary@gmail.com>
2023-01-20 18:24:12 -0500

    deploy testing

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
+require('dotenv').config();
 
 const databaseName = pkg.name;
 // pkg.name + (process.env.NODE_ENV === "test" ? "_test" : "");
@@ -13,13 +14,13 @@ if (process.env.LOGGING === "true") {
 }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if (process.env.DATABASE_URL) {
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
-}
+// if (process.env.DATABASE_URL) {
+//   config.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
 
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
